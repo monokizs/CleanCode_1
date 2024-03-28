06b4f3c28095ca0dc3d8b724d2c95ffb06535c00
mozso <126395568+m0zs0@users.noreply.github.com>
2024-03-28 13:44:49 +0100

    Funkcióbővítés

--- src/calculator.ts
+++ src/calculator.ts
@@ -3,5 +3,24 @@ export class Calculator {
     public Add(a: number, b: number): number {
         return a + b;
     }
+    
+    public Sub(a: number, b: number): number {
+        return a - b;
+    }
 
+    public Mul(a: number, b: number): number {
+        return a * b;
+    }
+
+    public Div(a: number, b: number): number {
+        return a / b;
+    }
+
+    public Sqrt(a: number): number {
+        return Math.sqrt(a);
+    }
+
+    public Pow(a: number, b: number): number {
+        return Math.pow(a,b);
+    }
 }
